92f85de359b42d2fbd6b5681a7b3eae1fcca1430
niranjan <niranjanbhor2000@gmail.com>
2023-01-03 21:48:31 +0530

    first commit[niranjan]

---
+++ mysite/myapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def sinngale_function(request):
+    return HttpResponse('Hello My nname is niranjan bhor')
